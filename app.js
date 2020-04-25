538ff0bc362f0ede7de20b6f92428861ba03cd3f
Jonny Chow <jonnyxchow@gmail.com>
2020-04-24 22:16:35 -0400

    Added url

--- app.js
+++ app.js
@@ -108,8 +108,12 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `You are searching for: "${received_message.text}".`
     }
+    url = "https://www.roguefitness.com/rogue-color-echo-bumper-plate";
+    $.get(url, function( data ) {
+    console.log(data);     
+    });
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
