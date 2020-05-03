1d4ec900d45d017756db4586193ed7b2faf34a51
Jonny Chow <jonnyxchow@gmail.com>
2020-05-03 19:52:41 -0400

    Added urls to another js file

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const request = require('request'),
   cheerio = require('cheerio'),
   body_parser = require('body-parser'),
   path = require('path'),
-  app = express().use(body_parser.json());
+  app = express().use(body_parser.json()),
+  search_urls = require('./item-urls')
 // creates express http server
 
 var interval_id = null;
@@ -40,142 +41,6 @@ var delay = 10;
 // Limit of iteems
 var item_limit = 4;
 
-// Item url dictionary
-var search_urls = { // Plate URLs
-  "plate hi-temp": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-hi-temp-bumper-plates"
-  },
-  "plate hg2": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-hg-2-0-bumper-plates"
-  },
-  "plate comp lb": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-competition-plates"
-  },
-  "plate comp lb2": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-color-lb-training-2-0-plates"
-  },
-  "plate black lb": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-black-training-lb-color-stripe-plates"
-  },
-  "plate black lb2": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-lb-training-2-0-plates"
-  },
-  "plate echo": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-echo-bumper-plates-with-white-text"
-  },
-  "plate echo color": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-color-echo-bumper-plate"
-  },
-  "plate machined": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-machined-olympic-plates"
-  },
-  "plate fleck": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-fleck-plates"
-  },
-  "plate olympic": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-olympic-plates"
-  },
-  "plate cal": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-calibrated-lb-steel-plates"
-  },
-  "plate change": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-lb-change-plates"
-  },
-  // Barbell URLs
-  // Ohio Bar
-  "barbell op oxide": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-ohio-bar-black-oxide"
-  },
-  "barbell op zinc": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/the-ohio-bar-black-zinc"
-  },
-  "barbell op ecoat": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/the-ohio-bar-2-0-e-coat"
-  },
-  "barbell op ss": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/stainless-steel-ohio-bar"
-  },
-  "barbell op cerakote": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/the-ohio-bar-cerakote"
-  },
-  // Ohio Power Bar
-  "barbell opb steel": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-45lb-ohio-power-bar-bare-steel"
-  },
-  "barbell opb ecoat": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-ohio-power-bar-e-coat"
-  },
-  "barbell opb zinc": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-45lb-ohio-power-bar-black-zinc"
-  },
-  "barbell opb ss": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-45lb-ohio-power-bar-stainless"
-  },
-  "barbell opb cerakote": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-45lb-ohio-powerlift-bar-cerakote"
-  },
-  // Echo bike
-  "bike echo": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-echo-bike"
-  },
-  // Bench
-  "bench flat 2.0": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-flat-utility-bench"
-  },
-  "bench adj 2.0": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-adjustable-bench-2-0"
-  },
-  "bench ab2": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/ab-2-adjustable-bench"
-  },
-  "bench ab3": {
-    "type": "multi",
-    "link": "https://www.roguefitness.com/rogue-ab-3-adjustable-bench"
-  },
-  "bench thompson": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/thompson-fatpad"
-  },
-  "bench fatpad": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-competition-fat-pad"
-  },
-  "bench foldup": {
-    "type": "single",
-    "link": "https://www.roguefitness.com/rogue-fold-up-utility-bench"
-  },
-  // Boneyard
-  "bone 29": "https://www.roguefitness.com/rogue-29mm-boneyard-bars",
-  "bone 28.5": "https://www.roguefitness.com/rogue-28-5-mm-boneyard-bars",
-  "bone dead": "https://www.roguefitness.com/rogue-boneyard-ohio-deadlift-bar"
-};
 
 app.use(express.static(__dirname + '/views/'));
 
@@ -294,7 +159,7 @@ function getData(search_url, rec_msg, callback) {
         return callback(error)
       };
       if (!error && response.statusCode == 200) {
-        console.log("Web scraping data from: " + search_url);
+        // console.log("Web scraping data from: " + search_url);
         let $ = cheerio.load(html);
         var items = [];
         // Check if search string already exists
---
+++ item-urls.js
@@ -0,0 +1,137 @@
+// Item url dictionary
+var search_urls = { // Plate URLs
+    "plate hi-temp": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-hi-temp-bumper-plates"
+    },
+    "plate hg2": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-hg-2-0-bumper-plates"
+    },
+    "plate comp lb": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-competition-plates"
+    },
+    "plate comp lb2": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-color-lb-training-2-0-plates"
+    },
+    "plate black lb": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-black-training-lb-color-stripe-plates"
+    },
+    "plate black lb2": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-lb-training-2-0-plates"
+    },
+    "plate echo": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-echo-bumper-plates-with-white-text"
+    },
+    "plate echo color": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-color-echo-bumper-plate"
+    },
+    "plate machined": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-machined-olympic-plates"
+    },
+    "plate fleck": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-fleck-plates"
+    },
+    "plate olympic": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-olympic-plates"
+    },
+    "plate cal": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-calibrated-lb-steel-plates"
+    },
+    "plate change": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-lb-change-plates"
+    },
+    // Barbell URLs
+    // Ohio Bar
+    "barbell op oxide": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-ohio-bar-black-oxide"
+    },
+    "barbell op zinc": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/the-ohio-bar-black-zinc"
+    },
+    "barbell op ecoat": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/the-ohio-bar-2-0-e-coat"
+    },
+    "barbell op ss": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/stainless-steel-ohio-bar"
+    },
+    "barbell op cerakote": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/the-ohio-bar-cerakote"
+    },
+    // Ohio Power Bar
+    "barbell opb steel": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-45lb-ohio-power-bar-bare-steel"
+    },
+    "barbell opb ecoat": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-ohio-power-bar-e-coat"
+    },
+    "barbell opb zinc": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-45lb-ohio-power-bar-black-zinc"
+    },
+    "barbell opb ss": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-45lb-ohio-power-bar-stainless"
+    },
+    "barbell opb cerakote": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-45lb-ohio-powerlift-bar-cerakote"
+    },
+    // Echo bike
+    "bike echo": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-echo-bike"
+    },
+    // Bench
+    "bench flat 2.0": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-flat-utility-bench"
+    },
+    "bench adj 2.0": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-adjustable-bench-2-0"
+    },
+    "bench ab2": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/ab-2-adjustable-bench"
+    },
+    "bench ab3": {
+      "type": "multi",
+      "link": "https://www.roguefitness.com/rogue-ab-3-adjustable-bench"
+    },
+    "bench thompson": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/thompson-fatpad"
+    },
+    "bench fatpad": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-competition-fat-pad"
+    },
+    "bench foldup": {
+      "type": "single",
+      "link": "https://www.roguefitness.com/rogue-fold-up-utility-bench"
+    },
+    // Boneyard
+    "bone 29": "https://www.roguefitness.com/rogue-29mm-boneyard-bars",
+    "bone 28.5": "https://www.roguefitness.com/rogue-28-5-mm-boneyard-bars",
+    "bone dead": "https://www.roguefitness.com/rogue-boneyard-ohio-deadlift-bar"
+  };
+module.exports = search_urls;
