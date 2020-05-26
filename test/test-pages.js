168ab097de2ea7e5c7ff0db8eb6a61a9f8efc057
Jonny Chow <jonnyxchow@gmail.com>
2020-05-26 18:44:01 -0400

    Added all tests for status of pages

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,16 +1,46 @@
-var expect  = require('chai').expect;
+var expect = require('chai').expect;
 var request = require('request');
 
-it('Main page status', function(done) {
-    request('http://localhost:3000' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
+describe('Status of pages', function () {
+    it('Index page status', function (done) {
+        request('http://localhost:3000', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
-});
 
-it('Bot guide page content', function(done) {
-    request('http://localhost:3000/bot-guide' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
+    it('Bot guide page content', function (done) {
+        request('http://localhost:3000/bot-guide', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Current items page content', function (done) {
+        request('http://localhost:3000/current-items', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Privacy policy page content', function (done) {
+        request('http://localhost:3000/privacy-policy', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Stock updates page content', function (done) {
+        request('http://localhost:3000/stock-updates', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Terms page content', function (done) {
+        request('http://localhost:3000/terms', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
 });
