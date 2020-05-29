26f0cb9d60e60a200c78248f8d9d429d8b3bd644
Jonny Chow <jonnyxchow@gmail.com>
2020-05-29 01:18:22 -0400

    Added boneyard tests

--- test/test_barbell.js
+++ test/test_barbell.js
@@ -6,7 +6,7 @@ var should = require('should');
 var axios = require('axios');
 var afterLoad = require('after-load');
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true });
+var nightmare = Nightmare({ show: false });
 
 describe('Barbell', function () {
     it(`Test product name`, function (done) {
@@ -29,20 +29,20 @@ describe('Barbell', function () {
         expect($('.product-options-bottom button').text()).to.have.string(`Notify Me`);
         done();
     });
-    it(`Test in stock request`, function (done) {
-        nightmare
-            .goto('https://www.roguefitness.com/rogue-45lb-ohio-power-bar-stainless')
-            .wait(15000)
-            .evaluate(function () {
-                return document.body.innerHTML;
-            })
-            .then(function (body) {
-                var $ = cheerio.load(body);
-                expect($('.product-options-bottom button').text()).to.have.string(`Notify Me`);
+    // it(`Test in stock request`, function (done) {
+    //     nightmare
+    //         .goto('https://www.roguefitness.com/rogue-45lb-ohio-power-bar-stainless')
+    //         .wait(15000)
+    //         .evaluate(function () {
+    //             return document.body.innerHTML;
+    //         })
+    //         .then(function (body) {
+    //             var $ = cheerio.load(body);
+    //             expect($('.product-options-bottom button').text()).to.have.string(`Notify Me`);
 
-            })
-            done();
-    });
+    //         })
+    //         done();
+    // });
     // it(`Test in stock`, function (done) {
     //     afterLoad('https://www.roguefitness.com/rogue-45lb-ohio-power-bar-stainless', function (html) {
     //         let $ = cheerio.load(html);
--- test/test_bone.js
+++ test/test_bone.js
@@ -6,12 +6,12 @@ var should = require('should');
 var axios = require('axios');
 var afterLoad = require('after-load');
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true });
+var nightmare = Nightmare({ show: false });
 
-describe('Barbell', function () {
+describe('Bone 29 barbell', function () {
     it(`Test product name`, function (done) {
         let $ = cheerio.load(fs.readFileSync('test/test_html/bone29.html'));
-        expect($('.product-title').text()).to.equal(`Boneyard Rogue 45LB Ohio Power Bar - Bare Steel with (Raw Shaft / Raw Sleeve)`);
+        expect($('.product-title').text()).to.equal(`Rogue 29MM Boneyard Bars`);
         done();
     });
     it(`Test price`, function (done) {
@@ -21,7 +21,28 @@ describe('Barbell', function () {
     });
     it(`Test in stock local`, function (done) {
         let $ = cheerio.load(fs.readFileSync('test/test_html/bone29.html'));
-        expect($('.product-options-bottom button').text()).to.have.string(`Notify Me`);
+        expect($('.bin-stock-availability').text()).to.have.string(`Notify Me`);
+        done();
+    });
+});
+describe('Bone 28.5 barbell', function () {
+    it(`Test item props`, function (done) {
+        let $ = cheerio.load(fs.readFileSync('test/test_html/bone28.5.html'));
+        let items = [];
+        $('.grouped-item').each(function (index, element) {
+            items[index] = {};
+            items[index]['name'] = $(element).find('.item-name').text();
+            items[index]['price'] = $(element).find('.price').text();
+            items[index]['in_stock'] = $(element).find('.bin-stock-availability').text();
+          });
+        expect(items[0]['name']).to.equal(`Boneyard Rogue Ohio Bar 1.2 with (Black Zinc Shaft / Bright Zinc Sleeve)`);
+        expect(items[1]['name']).to.equal(`Boneyard 28.5MM Echo Bar 2.0 with (Black Zinc Shaft / Chrome Sleeve)`);
+
+        expect(items[0]['price']).to.equal(`$225.00`);
+        expect(items[1]['price']).to.equal(`$165.00`);
+
+        expect(items[0]['in_stock']).to.have.string(`Out of Stock`);
+        expect(items[1]['in_stock']).to.have.string(`Out of Stock`);
         done();
     });
 });
