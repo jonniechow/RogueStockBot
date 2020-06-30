b8ddd98accc5b2ab125e433e6fff1265b697adfa
Jonny Chow <jonnyxchow@gmail.com>
2020-06-30 02:17:03 -0400

    start test

--- sql/select.sql
+++ sql/select.sql
@@ -10,3 +10,7 @@ SELECT C.num_searches, S.* FROM searches S,
 (SELECT COUNT(*) num_searches from searches
 group by user_id) C
 WHERE item_name='plate cal';
+
+UPDATE items
+SET stock_count = 2
+WHERE short_name="plate fleck";
